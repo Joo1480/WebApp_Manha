595d9a42c24762660eacd26102243bd9b82211cf
Joo1480 <joabernardelli123@gmail.com>
2023-03-29 11:02:49 -0300

    Modificação da ClientesController

--- WebApp_Manha/Controllers/ClientesController.cs
+++ WebApp_Manha/Controllers/ClientesController.cs
@@ -8,5 +8,34 @@ namespace WebApp_Manha.Controllers
         {
             return View();
         }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastro(string Nome, string Telefone)
+        {
+            if (string.IsNullOrEmpty(Nome))
+            {
+                TempData["Erro"] = "O campo Nome está vazio";
+            }
+            if (string.IsNullOrEmpty(Telefone))
+            {
+                TempData["Erro"] = "O campo Telefone está vazio";
+            }
+            return View();
+        }
+        public IActionResult Editar(int id)
+        {
+            return View();
+        }
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
+        public IActionResult Compras(int id)
+        {
+            return View();
+        }
     }
 }
