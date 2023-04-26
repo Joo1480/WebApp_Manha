5408ce9c7a38bd2ebd32a87f8d42e22c660944ff
JOAO BERNARDELLI <joaobernardelli123@gmail.com>
2023-04-26 08:51:35 -0300

    Conexão com o contexto

---
+++ WebApp_Manha/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp_Manha
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> opt)
+        {
+
+        }
+    }
+}
