6ae97f9a901f6bf24ba550d2b819aaf600485300
JOAO BERNARDELLI <joaobernardelli123@gmail.com>
2023-05-24 10:29:37 -0300

    Lista de produto

--- WebApp_Manha/Controllers/ProdutosController.cs
+++ WebApp_Manha/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp_Manha.Entidades;
 using WebApp_Manha.Models;
 
@@ -15,7 +16,10 @@ namespace WebApp_Manha.Controllers
         
         public IActionResult Lista()
         {
-            return View(db.PRODUTOS.ToList());
+            List<Produtos> model = new List<Produtos>();
+            model = db.PRODUTOS.Include(x => x.CATEGORIA).ToList();
+
+            return View(model);
         }
         public IActionResult Cadastro()
         {
